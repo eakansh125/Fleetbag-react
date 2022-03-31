61bf67b1d8b3d0c182d13d4da02d9efdd4d5c4a2
Eakansh <you@example.com>
2022-04-01 00:47:43 +0530

    fix: fixed a deployment bug

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,24 +8,24 @@ function Home() {
     <header class="nav-header">
         <div class="nav-header-div">
             <div class="nav-header-div-lt">
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#">HOME</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#">FILTER</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#">PRODUCTS</a>
+                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">HOME</a>
+                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">FILTER</a>
+                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">PRODUCTS</a>
             </div>
             <div class="nav-header-div-mid">
                 <input class="input-outlined" type="text" placeholder="Search" />
             </div>
             <div class="nav-header-div-rt">   
-                        <a href="#" class="btn-small margin-lr">Login</a>
+                        <a href="#top" class="btn-small margin-lr">Login</a>
 
                         <div class="badge-container margin-lr">
-                            <a href="#"><img
+                            <a href="#top"><img
                                 src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/30/000000/external-like-instagram-flatart-icons-outline-flatarticons.png" alt="icons"/>
                             <span class="badge-span">6</span></a>
                         </div>
 
                         <div class="badge-container margin-lr">
-                            <a href="#"><img src="https://img.icons8.com/ios/30/000000/add-shopping-cart--v1.png" alt="icons"/>
+                            <a href="#top"><img src="https://img.icons8.com/ios/30/000000/add-shopping-cart--v1.png" alt="icons"/>
                                 <span class="badge-span">8</span></a>
                         </div> 
             </div>
