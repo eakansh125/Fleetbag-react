070379ec1e412300e6428b559e309c6ce991baeb
Eakansh <you@example.com>
2022-04-01 01:08:44 +0530

    feat: added Nav component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,3 +1,4 @@
+import Navbar from '../../Reusables/Navbar';
 import Deals from './Deals';
 import Featured from './Featured';
 import './Home.css'
@@ -5,33 +6,7 @@ import './Home.css'
 function Home() {
   return (
     <body>
-    <header class="nav-header">
-        <div class="nav-header-div">
-            <div class="nav-header-div-lt">
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">HOME</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">FILTER</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">PRODUCTS</a>
-            </div>
-            <div class="nav-header-div-mid">
-                <input class="input-outlined" type="text" placeholder="Search" />
-            </div>
-            <div class="nav-header-div-rt">   
-                        <a href="#top" class="btn-small margin-lr">Login</a>
-
-                        <div class="badge-container margin-lr">
-                            <a href="#top"><img
-                                src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/30/000000/external-like-instagram-flatart-icons-outline-flatarticons.png" alt="icons"/>
-                            <span class="badge-span">6</span></a>
-                        </div>
-
-                        <div class="badge-container margin-lr">
-                            <a href="#top"><img src="https://img.icons8.com/ios/30/000000/add-shopping-cart--v1.png" alt="icons"/>
-                                <span class="badge-span">8</span></a>
-                        </div> 
-            </div>
-        </div>
-    </header>
-
+    <Navbar />
     <main class="main-view">
         <div class="main-view-container">
             
