b6374487a569f3f228e097640ba5abfd68ca330e
Eakansh <you@example.com>
2022-04-01 11:57:45 +0530

    fix: fixed className and Navbar issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import { Routes, Route } from "react-router-dom";
+import Navbar from './Reusables/Navbar';
+import Footer from './Reusables/Footer';
 
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
+    <Footer />
     </div>
   );
 }
--- src/Reusables/Navbar.js
+++ src/Reusables/Navbar.js
@@ -1,28 +1,28 @@
 
 function Navbar() {
     return (
-        <header class="nav-header">
-        <div class="nav-header-div">
-            <div class="nav-header-div-lt">
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">HOME</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">FILTER</a>
-                <a class="text-dec-none font-bold margin-lr font-primary-color" href="#top">PRODUCTS</a>
+        <header className="nav-header">
+        <div className="nav-header-div">
+            <div className="nav-header-div-lt">
+                <a className="text-dec-none font-bold margin-lr font-primary-color" href="#top">HOME</a>
+                <a className="text-dec-none font-bold margin-lr font-primary-color" href="#top">FILTER</a>
+                <a className="text-dec-none font-bold margin-lr font-primary-color" href="#top">PRODUCTS</a>
             </div>
-            <div class="nav-header-div-mid">
-                <input class="input-outlined" type="text" placeholder="Search" />
+            <div className="nav-header-div-mid">
+                <input className="input-outlined" type="text" placeholder="Search" />
             </div>
-            <div class="nav-header-div-rt">   
-                        <a href="#top" class="btn-small margin-lr">Login</a>
+            <div className="nav-header-div-rt">   
+                        <a href="#top" className="btn-small margin-lr">Login</a>
 
-                        <div class="badge-container margin-lr">
+                        <div className="badge-container margin-lr">
                             <a href="#top"><img
                                 src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/30/000000/external-like-instagram-flatart-icons-outline-flatarticons.png" alt="icons"/>
-                            <span class="badge-span">6</span></a>
+                            <span className="badge-span">6</span></a>
                         </div>
 
-                        <div class="badge-container margin-lr">
+                        <div className="badge-container margin-lr">
                             <a href="#top"><img src="https://img.icons8.com/ios/30/000000/add-shopping-cart--v1.png" alt="icons"/>
-                                <span class="badge-span">8</span></a>
+                                <span className="badge-span">8</span></a>
                         </div> 
             </div>
         </div>
